559d8ae6c9279700ec0da0ff45d7bfa2824aa0d9
TronkisR <73221082+TronkisR@users.noreply.github.com>
2022-03-18 15:37:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const API = "https://chat2022.gundegadekena.repl.co"  // nado ukazat svoj !!!!
 
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards')
 
     function sutitZinu()
     {
@@ -12,14 +13,14 @@ let zinas = document.querySelector('.chataZinas');
         
 
         zinas.innerHTML = zinas.innerHTML + '<br /> ' + zina.value;
-        fetch(API +'/sutit'+vards.value+'/'+zina.value)
+        fetch(API +'/sutit'+ vards.value + '/' +zina.value)
 
 
     }
     
     async function ieladetChataZinas()
     {
-        let datiNoServera = await fetch('chatazinas.txt');
+        let datiNoServera = await fetch(API + '/lasit');
         let dati = await datiNoServera.text();
         zinas.innerHTML = dati;
     }
@@ -27,7 +28,12 @@ let zinas = document.querySelector('.chataZinas');
 
     async function ieladetChataZinasJson()
     {
-        let datiNoServera = await fetch('chatazinas.txt');
-        let dati = await datiNoServera.text();
-        zinas.innerHTML = dati;
+        let datiNoServera = await fetch(API + '/lasit');
+        let dati = await datiNoServera.json();
+       i = 0; 
+       while ( i < await dati.lenght )
+       {
+            console.log( i )
+            i = i + 1;
+       }
     }
